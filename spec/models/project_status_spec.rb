f9d603f0f53fd31d8831b31ee0dc56bc34816efc
raphaelsattler <raphaelsattler@hotmail.com>
2018-12-09 20:36:32 -0200

    Fix indentation error

--- spec/models/project_status_spec.rb
+++ spec/models/project_status_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ProjectStatus, type: :model do
-
   it { is_expected.to validate_presence_of(:name) }
 
   it { is_expected.to validate_uniqueness_of(:name) }
