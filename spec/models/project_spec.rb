01c2706b0ca9f4f655322209043da0056b6f5de1
raphaelsattler <raphaelsattler@hotmail.com>
2018-12-09 19:32:09 -0200

    Create Project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe ProjectStatus, type: :model do
+RSpec.describe Project, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
 
   it { is_expected.to validate_uniqueness_of(:name) }
 
-  it { is_expected.to have_many(:projects) }
+  it { is_expected.to belong_to(:project_status) }
 end
