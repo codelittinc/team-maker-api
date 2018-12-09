f2cdb3eafd125ce1b023981510655f937a4e961a
raphaelsattler <raphaelsattler@hotmail.com>
2018-12-09 21:21:23 -0200

    Create Schedule model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Project, type: :model do
   it { is_expected.to validate_uniqueness_of(:name) }
 
   it { is_expected.to belong_to(:project_status) }
+
+  it { is_expected.to have_many(:schedules) }
 end
