59a903842754145ae73ec82c474feedd6c93f79e
Raphael Sattler <raphaelsattler@hotmail.com>
2018-12-18 15:34:56 -0200

    Refactor the test to explicit all attributes that needs to be shown on the page

--- spec/controller/resources_controller_spec.rb
+++ spec/controller/resources_controller_spec.rb
@@ -26,16 +26,23 @@ RSpec.describe ResourcesController, type: :controller do
       role = Role.create!(name: 'Engineer')
       resources_type = ResourceType.create!(name: 'Employee')
       @resource = Resource.create!(name: 'Kaio Cristian',
-                       role_id: role.id,
-                       resource_type_id: resources_type.id)
+                                   role_id: role.id,
+                                   resource_type_id: resources_type.id)
 
       get :show, params: { id: @resource.id }
+    end
+
+    let(:reference_hash) do
+      { id: @resource.id,
+        name: @resource.name,
+        role_id: @resource.role_id,
+        resource_type_id: @resource.resource_type_id }
     end
 
     it { expect(response.body).to look_like_json }
 
     it { expect(body_as_json.keys).to match_array(%w[id name role_id resource_type_id]) }
 
-    it { expect(body_as_json).to match(@resource.attributes) }
+    it { expect(body_as_json).to match(reference_hash) }
   end
 end
