ebf3bcd79cebf14dc85af69bbde80e1c5e444ed4
Raphael Sattler <raphaelsattler@hotmail.com>
2018-12-19 09:33:15 -0200

    Create Resource create

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -13,9 +13,23 @@ class ResourcesController < ApplicationController
     render json: { error: error }
   end
 
+  def create
+    @resource = Resource.new(resource_params)
+
+    if @resource.save
+      render json: @resource, status: :created, location: @resource
+    else
+      render json: @resource.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def resource_id_params
     params.permit(:id)
+  end
+
+  def resource_params
+    params.require(:resource).permit(:name, :role_id, :resource_type_id)
   end
 end
