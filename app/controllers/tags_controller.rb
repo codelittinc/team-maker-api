615b8c100294defaf9fa6ffb3a4e85ab9ca23ffd
Raphael Sattler <raphaelsattler@hotmail.com>
2018-12-21 13:26:37 -0200

    Create Tag create action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,10 +13,24 @@ class TagsController < ApplicationController
     render json: { error: error }
   end
 
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      render json: @tag, status: :created, location: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def tag_id_params
     params.permit(:id)
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name, :resource_id)
   end
 
   def tag
