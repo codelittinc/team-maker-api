660e224c7bf364e438e0c7da32ba9235b78447e8
Raphael Sattler <raphaelsattler@hotmail.com>
2018-12-21 13:25:51 -0200

    Add automated tests for Tag create feature

--- spec/controller/tags_controller_spec.rb
+++ spec/controller/tags_controller_spec.rb
@@ -26,8 +26,8 @@ RSpec.describe TagsController, type: :controller do
     let(:role) { Role.create!(name: 'Engineer') }
     let(:resources_type) { ResourceType.create!(name: 'Employee') }
     let(:resource) { Resource.create!(name: 'Kaio Cristian',
-                                  role_id: role.id,
-                                  resource_type_id: resources_type.id) }
+                                      role_id: role.id,
+                                      resource_type_id: resources_type.id) }
     let(:tag) { Tag.create!(name: 'Ruby', resource_id: resource.id) }
 
     let(:reference_hash) do
@@ -38,6 +38,32 @@ RSpec.describe TagsController, type: :controller do
 
     before do
       get :show, params: { id: tag.id }
+    end
+
+    it { expect(response.body).to look_like_json }
+
+    it { expect(body_as_json.keys).to match_array(%w[id name resource_id]) }
+
+    it { expect(body_as_json).to match(reference_hash) }
+  end
+
+  describe 'GET #create' do
+    let(:role) { Role.create!(name: 'Engineer') }
+    let(:resources_type) { ResourceType.create!(name: 'Employee') }
+    let(:resource) { Resource.create!(name: 'Kaio Cristian',
+                                      role_id: role.id,
+                                      resource_type_id: resources_type.id) }
+
+    let(:tag) { { name: 'Ruby', resource_id: resource.id } }
+
+    let(:reference_hash) do
+      { id: Tag.last.id,
+        name: 'Ruby',
+        resource_id: resource.id }
+    end
+
+    before do
+      get :create, params: { tag: tag }
     end
 
     it { expect(response.body).to look_like_json }
