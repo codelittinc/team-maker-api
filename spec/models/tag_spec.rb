38285cfc4c06b5405f0179fd98d0c15c47fdf42c
raphaelsattler <raphaelsattler@hotmail.com>
2018-12-24 15:36:32 -0200

    Add automated tests to ResourceTag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Tag, type: :model do
   it { is_expected.to validate_presence_of(:name) }
 
-  it { is_expected.to have_many(:resources) }
+  it { is_expected.to have_many(:resource_tags) }
 end
