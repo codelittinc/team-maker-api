56a6ab80ed07230854c97501801b4c0145bc6cf6
raphaelsattler <raphaelsattler@hotmail.com>
2019-01-01 20:46:36 -0200

    Create automated tests for Schedule show

--- spec/integration/schedules_spec.rb
+++ spec/integration/schedules_spec.rb
@@ -24,4 +24,32 @@ describe 'Schedules API' do
       end
     end
   end
+
+  path '/schedules/{id}' do
+    get 'Retrieves a schedule' do
+      tags 'Schedules'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'OK' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 project_id: { type: :integer },
+                 resource_id: { type: :integer },
+                 schedule_type_id: { type: :integer }
+               },
+               required: %w[id project_id resource_id schedule_type_id]
+
+        let(:id) { create(:schedule).id }
+
+        run_test!
+      end
+
+      response '200', "Couldn't find Schedule with 'id'=#{id}" do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
 end
