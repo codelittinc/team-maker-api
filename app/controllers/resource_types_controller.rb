205382e1013b56759d94312d8eae9edfad226bb1
raphaelsattler <raphaelsattler@hotmail.com>
2019-03-09 19:19:59 -0300

    Create resource type create action and a method to permit params

--- app/controllers/resource_types_controller.rb
+++ app/controllers/resource_types_controller.rb
@@ -13,10 +13,24 @@ class ResourceTypesController < ApplicationController
     render json: { error: error }
   end
 
+  def create
+    @resource_type = ResourceType.new(resource_type_params)
+
+    if @resource_type.save
+      render json: @resource_type, status: :created, location: @resource_type
+    else
+      render json: @resource_type.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def resource_type_id_params
     params.permit(:id)
+  end
+
+  def resource_type_params
+    params.require(:resource_type).permit(:name)
   end
 
   def resource_type
