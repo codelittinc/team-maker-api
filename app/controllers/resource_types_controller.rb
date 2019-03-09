9104b365073ae55f215ff59a0b7934c2d1079a57
raphaelsattler <raphaelsattler@hotmail.com>
2019-03-09 17:27:34 -0300

    Create resource type show action and a method to find a resource type

--- app/controllers/resource_types_controller.rb
+++ app/controllers/resource_types_controller.rb
@@ -4,4 +4,22 @@ class ResourceTypesController < ApplicationController
 
     render json: @resource_types
   end
+
+  def show
+    @resource_type = resource_type
+
+    render json: @resource_type
+  rescue StandardError => error
+    render json: { error: error }
+  end
+
+  private
+
+  def resource_type_id_params
+    params.permit(:id)
+  end
+
+  def resource_type
+    ResourceType.find(resource_type_id_params[:id])
+  end
 end
